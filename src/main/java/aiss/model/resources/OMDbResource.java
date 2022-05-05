81bc447e2f07a705285b498a7f1b19134af0c0f3
gabrielvcg <90857860+Gabrielvcg@users.noreply.github.com>
2022-05-05 18:11:39 +0200

    Update OMDbResource.java

--- src/main/java/aiss/model/resources/OMDbResource.java
+++ src/main/java/aiss/model/resources/OMDbResource.java
@@ -14,10 +14,16 @@ public class OMDbResource {
 	private static final String OMDB_API_KEY = "c68bd62c";  // TODO: Change this API KEY for your personal Key
 	private static final Logger log = Logger.getLogger(OMDbResource.class.getName());
 	
+	
 	public MovieSearch getMovies(String query) throws UnsupportedEncodingException {
 
-		// TODO: Perform search in OMDb
+		String texto=URLEncoder.encode(query,"UTF-8");
+		String uri="http://www.omdbapi.com/?apikey="+OMDB_API_KEY+"&s="+texto;
+		log.log(Level.FINE,"MOVIE URI: "+ uri);
+		ClientResource cr= new ClientResource(uri);
+		MovieSearch movieSearch=cr.get(MovieSearch.class);
 		
-	    return null;
+	    return movieSearch;
+	}
 	}
 }
